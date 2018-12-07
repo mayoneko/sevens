d99d83b0c9264736fd1ee73c986ec735163a40ad
mayoneko <ev60622@meiji.ac.jp>
2018-12-08 02:11:03 +0900

    refactor(kt): Add Cards Class and Edit (Board and User) Class

--- src/main/kotlin/mayoneko/Sevens.kt
+++ src/main/kotlin/mayoneko/Sevens.kt
@@ -2,31 +2,46 @@ package mayoneko
 
 
 fun main(args: Array<String>) {
-    val board= Board()
-
+    val cards = Cards()
+    val board = Board(cards)
     val users = mutableListOf<User>()
     for (i in 0..2) {
-        users.add(User())
+        users.add(User(cards,i+1))
     }
-    var index = 0
-    (0..51).toList().shuffled().forEach {
-        users[index].cards.add(Card(it % 4, it % 13 + 1))
-        index = ++index % 3
-    }
+    board.viewCards()
     users.map {
         it.viewCards()
     }
-
 }
 
 
-data class Card(val suitNum: Int, val num: Int)
+data class Card(val suitNum: Int, val num: Int, val ownerID: Int)
 
 enum class Suit(val suitNum: Int) {
     HEARTS(0), DIAMONDS(1), CLUBS(2), SPADES(3)
 }
 
-class User : Board() {
+enum class Owner(val ownerID: Int) {
+    BOARD(0), USER1(1), USER2(2), USER3(3)
+}
+
+class Cards {
+    val cards: MutableList<Card>
+
+    init {
+        val ownerIDArray = (0..51).map { it % 3 + 1 }.shuffled()
+        cards = mutableListOf()
+        for (i in 0..51) {
+            cards.add(Card(i % 4, i % 13 + 1, ownerIDArray[i]))
+        }
+    }
+
+}
+
+class User(_cards: Cards, _ownerID: Int) : Board(_cards) {
+    override val cards: Cards = _cards
+    override val ownerID = _ownerID
+
     var passCount: Int = 0
     var canPass: Boolean = true
 
@@ -43,26 +58,20 @@ class User : Board() {
     }
 }
 
-open class Board {
-    val cards: MutableList<Card> = mutableListOf()
+open class Board(_cards: Cards) {
+    open val cards: Cards = _cards
+    open val ownerID = 0
 
-    fun sortCards(): List<Card> {
-        return this.cards.sortedWith(compareBy(
-            { it.suitNum },
-            { it.num }
-        ))
-    }
-
-    fun viewCard(card:Card):Pair<String,String>{
+    private fun viewCard(card: Card): Pair<String, String> {
         return Pair(
-            when(card.suitNum){
+            when (card.suitNum) {
                 0 -> "♥"
                 1 -> "♦"
                 2 -> "♣"
                 3 -> "♠"
                 else -> card.suitNum.toString()
             },
-            when(card.num){
+            when (card.num) {
                 1 -> "A"
                 10 -> "0"
                 11 -> "J"
@@ -74,24 +83,16 @@ open class Board {
     }
 
     fun viewCards() {
-        val sortedCards = sortCards()
-        var fullBoard= ""
-        var index=0
-        for (i in 0..51){
-            if(index<sortedCards.size){
-                val card = sortedCards[index]
-
-                if(card.suitNum*13+(card.num-1)==i){
-                    fullBoard+="{${viewCard(card).first}:${viewCard(card).second}}"
-                    index++
-                }else{
-                    fullBoard+="{-:-}"
-                }
-            }else{
-                fullBoard+="{-:-}"
+        var fullBoard = ""
+        for (i in 0..51) {
+            val card = this.cards.cards[i]
+            fullBoard += if (card.ownerID == this.ownerID) {
+                "{${viewCard(card).first}:${viewCard(card).second}}"
+            } else {
+                "{-:-}"
             }
-            if(i%13==12){
-                fullBoard+="\n"
+            if (i % 13 == 12) {
+                fullBoard += "\n"
             }
         }
         println(fullBoard)
