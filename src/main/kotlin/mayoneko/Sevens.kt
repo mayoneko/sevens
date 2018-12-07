a2c71b7bc1f5ab95185eebbf5661a97eada2c347
mayoneko <ev60622@meiji.ac.jp>
2018-12-07 13:35:48 +0900

    feat(kt): Add Board Class

--- src/main/kotlin/mayoneko/Sevens.kt
+++ src/main/kotlin/mayoneko/Sevens.kt
@@ -2,6 +2,7 @@ package mayoneko
 
 
 fun main(args: Array<String>) {
+    val board= Board()
 
     val users = mutableListOf<User>()
     for (i in 0..2) {
@@ -13,7 +14,7 @@ fun main(args: Array<String>) {
         index = ++index % 3
     }
     users.map {
-        println(it.cards)
+        it.viewCards()
     }
 
 }
@@ -25,9 +26,7 @@ enum class Suit(val suitNum: Int) {
     HEARTS(0), DIAMONDS(1), CLUBS(2), SPADES(3)
 }
 
-class User {
-    val cards: MutableList<Card?> = mutableListOf()
-
+class User : Board() {
     var passCount: Int = 0
     var canPass: Boolean = true
 
@@ -41,5 +40,60 @@ class User {
 
     fun playingAlgorithm() {
         //TODO
+    }
+}
+
+open class Board {
+    val cards: MutableList<Card> = mutableListOf()
+
+    fun sortCards(): List<Card> {
+        return this.cards.sortedWith(compareBy(
+            { it.suitNum },
+            { it.num }
+        ))
+    }
+
+    fun viewCard(card:Card):Pair<String,String>{
+        return Pair(
+            when(card.suitNum){
+                0 -> "♥"
+                1 -> "♦"
+                2 -> "♣"
+                3 -> "♠"
+                else -> card.suitNum.toString()
+            },
+            when(card.num){
+                1 -> "A"
+                10 -> "0"
+                11 -> "J"
+                12 -> "Q"
+                13 -> "K"
+                else -> card.num.toString()
+            }
+        )
+    }
+
+    fun viewCards() {
+        val sortedCards = sortCards()
+        var fullBoard= ""
+        var index=0
+        for (i in 0..51){
+            if(index<sortedCards.size){
+                val card = sortedCards[index]
+
+                if(card.suitNum*13+(card.num-1)==i){
+                    fullBoard+="{${viewCard(card).first}:${viewCard(card).second}}"
+                    index++
+                }else{
+                    fullBoard+="{-:-}"
+                }
+            }else{
+                fullBoard+="{-:-}"
+            }
+            if(i%13==12){
+                fullBoard+="\n"
+            }
+        }
+        println(fullBoard)
     }
 }
